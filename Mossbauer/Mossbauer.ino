f39e11475c6fb4141fbc32bab8eab9a61efcd126
Louise <rowanandlouise@gmail.com>
2020-08-03 14:04:58 -0500

    interval timer for serial output

--- Mossbauer/Mossbauer.ino
+++ Mossbauer/Mossbauer.ino
@@ -2,22 +2,29 @@
 #include <ADC_util.h>
 
 // INIT constants for IO pins:
-int sampleHoldPin = A9;
-int comparatorPin = 21;
-int resetPin = 22;
+const int sampleHoldPin = A9;
+const int comparatorPin = 21;
+const int resetPin = 22;
 //int piezoPin = A14;
   
 // INIT variables:
-//volatile bool serialFlag = false;
+volatile bool serialFlag = false;
 uint16_t adcValue;
+volatile uint16_t phase = 0;
+
 uint32_t pha[4096];
 uint32_t mcs[1024];
-volatile uint16_t phase = 0;
+uint32_t phaCopy[4096];
+uint32_t mcsCopy[1024];
+
+// INIT constants:
+const uint16_t serialInterval = 5; // in seconds
 
 // INIT ADC
 ADC *adc = new ADC();
 // INIT phaseTimer (as IntervalTimer)
 // INIT serialTimer (as IntervalTimer)
+IntervalTimer serialTimer;
 
 
 volatile uint32_t start = 0;
@@ -40,6 +47,7 @@ void setup() {
 
   // Attach phaseTimer to call phase_isr every 100?? us
   // Attach serialTimer to call serial_isr every 5?? min
+  serialTimer.begin(serial_isr, serialInterval*1000000);
   // Attach hardware interrupt to call pulse_isr on falling edge of comparatorPin
   attachInterrupt(digitalPinToInterrupt(comparatorPin), pulse_isr, FALLING);
 
@@ -48,6 +56,11 @@ void setup() {
     // pulse_isr: <112
     // serial_isr: 128
     // (serial1 = 64, usb = 112, default = 128) (all multiples of 16, 0 - 255)
+    
+  //NVIC_SET_PRIORITY(IRQ_PORTD, 96) // works without this line
+  // Port assignments: https://forum.pjrc.com/threads/23950-Parallel-GPIO-on-Teensy-3-0?p=34158&viewfull=1#post34158
+  serialTimer.priority(240);
+  
 
   // Reset sample and hold circuit
   pinMode(resetPin, OUTPUT);
@@ -57,37 +70,39 @@ void setup() {
 }
 
 void loop() {
-  // IF serialFlag is true:
-    // Disable interrupts
-    // Make copy of pha and mcs
-    // Enable interrupts
-    // Output pha and mcs copies with Serial
-    // SET serialFlag to false
+  if (serialFlag) {
+    noInterrupts();
+    memcpy(phaCopy, pha, sizeof phaCopy);
+    memcpy(mcsCopy, mcs, sizeof mcsCopy);
+    interrupts();
 
-  /*
-  for (int i = 0; i < 4096; i++) {
-    Serial.print(i);
-    Serial.print(":  ");
-    Serial.println(pha[i]);
+    // OUTPUT pha array
+    for (int i = 0; i < 4096; i++) {
+      Serial.print(i);
+      Serial.print(":   ");
+      Serial.println(phaCopy[i]);
+    }
+    //OUTPUT mcs array
+    for (int i = 0; i < 1024; i++) {
+      Serial.print(i);
+      Serial.print(":   ");
+      Serial.println(mcsCopy[i]);
+    }
+    
+    serialFlag = false;
   }
-  delay(5000);
-  */
 }
 
 void pulse_isr() {
-  // SET adcValue to analogRead
   adcValue = analogRead(sampleHoldPin);
   //Serial.println(adcValue);
 
   // Reset sample and hold circuit
   pinMode(resetPin, OUTPUT);
   digitalWrite(resetPin, LOW);
-  //delayMicroseconds(5);
+  //delayMicroseconds(5); // Add delay if necessary
   pinMode(resetPin, INPUT);
 
-  // INCREMENT pha at adcValue index
-  // IF adcValue is within thresholds:
-    // INCREMENT mcs at phase index
   pha[adcValue]++;
   mcs[phase]++;
 }
@@ -101,5 +116,5 @@ void phase_isr() {
 }
 
 void serial_isr() {
-  // SET serialFlag to true
+  serialFlag = true;
 }
