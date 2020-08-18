0b1e8b5a95a9549d4d54b826230ecfa8943925c9
Louise <rowanandlouise@gmail.com>
2020-08-18 14:42:28 -0500

    overflow check and fixed disable interrupts

--- Mossbauer/Mossbauer.ino
+++ Mossbauer/Mossbauer.ino
@@ -37,7 +37,6 @@ volatile uint32_t end = 0;
 
 void setup() {
   Serial.begin(115200);
-  //while (!Serial) {}
   
   // Generate pos/vel look up tables
   float w = 0;
@@ -81,35 +80,43 @@ void setup() {
   // Attach hardware interrupt to call pulse_isr on falling edge of comparatorPin
   attachInterrupt(digitalPinToInterrupt(comparatorPin), pulse_isr, FALLING);
 
-  // SET interrupt priorities:
-    // phase_isr: <112
-    // pulse_isr: <112
-    // serial_isr: 128
-    // (serial1 = 64, usb = 112, default = 128) (all multiples of 16, 0 - 255)
-    
-  NVIC_SET_PRIORITY(IRQ_PORTD, 96); // works without this line
+  // SET interrupt priorities:  
+  NVIC_SET_PRIORITY(IRQ_PORTD, 96);
   // Port assignments: https://forum.pjrc.com/threads/23950-Parallel-GPIO-on-Teensy-3-0?p=34158&viewfull=1#post34158
+  // Priorities: https://forum.pjrc.com/threads/26397-Interrupt-priority-for-attachInterrupt#:~:text=Yes%2C%20on%20Teensy%20all%20the,%22%20priority%20in%20Cortex%20M).
+  // (usb serial is 112, hardware interrupt default is 128)
   serialTimer.priority(240);
   phaseTimer.priority(64);
   
-
   // Reset sample and hold circuit
   pinMode(resetPin, OUTPUT);
   digitalWrite(resetPin, LOW);
   delay(10);
   pinMode(resetPin, INPUT);
-
-
 }
 
 void loop() {
   if (serialFlag) {
     uint32_t phaCopy[4096];
     uint32_t mcsCopy[1024];
-    noInterrupts();
+    // Don't want to stop phase_isr, so use detachInterrupt() instead of noInterrupts()
+    detachInterrupt(digitalPinToInterrupt(comparatorPin)); // Only disable pulse_isr
+    
     memcpy(phaCopy, pha, sizeof phaCopy);
     memcpy(mcsCopy, mcs, sizeof mcsCopy);
-    interrupts();
+    memset(pha, 0, sizeof pha);
+    memset(mcs, 0, sizeof mcs);
+    
+    // May need to clear interrupt flag: https://forum.pjrc.com/threads/44889-How-to-clear-an-external-interrupt-s-status-flag-in-Teensyduino-for-Teensy-3-2
+    //PORTD_ISFR = CORE_PIN21_BITMASK; 
+    attachInterrupt(digitalPinToInterrupt(comparatorPin), pulse_isr, FALLING); // Reenable pulse_isr
+
+    // Copying and clearing the arrays takes ~390 us, so some pulses will be missed
+    // Reset sample and hold; missed pulses will leave sample and hold high, preventing comparator falling edge
+    pinMode(resetPin, OUTPUT);
+    digitalWrite(resetPin, LOW);
+    while (digitalRead(comparatorPin) == LOW ) {}
+    pinMode(resetPin, INPUT);
 
     // OUTPUT pha array
     for (int i = 0; i < 4096; i++) {
@@ -141,6 +148,12 @@ void pulse_isr() {
 
   pha[adcValue]++;
   mcs[velPhase]++;
+
+  // Check for overflow
+  // At 50 us between pulses, it takes at least 60 hrs to overflow uint32_t
+  if (pha[adcValue] >= 4294967200 || mcs[velPhase] >= 4294967200) { 
+    serialFlag = true;
+  }
 }
 
 void phase_isr() {
